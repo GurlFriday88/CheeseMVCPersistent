44cc997a58308335f46e989737406da49de69fad
GurlFriday88 <entrotlek@gmail.com>
2018-03-19 20:58:19 -0400

    added add controller and add controller override to add new category to db

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CheeseMVC.Data;
 using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -23,6 +24,35 @@ namespace CheeseMVC.Controllers
             List<CheeseCategory>cheeseCategories = context.Categories.ToList();
 
             return View(cheeseCategories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel newCategory = new AddCategoryViewModel();
+           
+
+            return View(newCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel formInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Add");
+
+            }
+
+            //if viewmodel passes validation create a new cheese category and add it to the datatbase context
+
+            CheeseCategory newCategory = new CheeseCategory
+            {
+                Name = formInput.Name
+            };
+
+            context.Categories.Add(newCategory);
+            context.SaveChanges();
+            return Redirect("/Category");
         }
     }
 }
