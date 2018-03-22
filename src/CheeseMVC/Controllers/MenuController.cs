24580534bc3f888ee7b5557abc452080f07398e3
GurlFriday88 <entrotlek@gmail.com>
2018-03-22 00:19:27 -0400

    added view menu action method to menu controller

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -2,6 +2,7 @@
 using CheeseMVC.Models;
 using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,5 +60,30 @@ namespace CheeseMVC.Controllers
 
             return Redirect("Menu/ViewMenu/" + newMenu.MenuID);
         }
+
+        [HttpGet]
+        [Route("/Menu/ViewMenu/id= ")]
+        public IActionResult ViewMenu(int id)
+        {
+            Menu targetMenu = context.Menus.Single(tm => tm.MenuID == id);
+
+            List<CheeseMenu> items = context
+            .CheeseMenus
+            .Include(item => item.Cheese)
+            .Where(cm => cm.MenuID == id)
+            .ToList();
+
+            ViewMenuViewModel menuView = new ViewMenuViewModel
+            {
+                Menu = targetMenu,
+                Items = items
+
+
+            };
+
+            return View(menuView);
+
+        }
     }
+
 }
