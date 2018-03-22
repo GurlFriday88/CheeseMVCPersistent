d198d2bb1852c186f1378fbc3843e7f279672ee7
GurlFriday88 <entrotlek@gmail.com>
2018-03-21 21:27:12 -0400

    added MenuController and declaired index action result and set up db context to be used in controller.

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+
+            context = dbContext;
+      
+
+        }
+
+        public IActionResult Index()
+        {
+
+            IList<Menu> Menus = context.Menus.ToList();
+
+            return View(Menus);
+
+        }
+    }
+}
